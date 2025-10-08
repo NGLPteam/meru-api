e2690aea3d1ea0601fea88eef531dfcb1056682a
Alexa Grey <alexa@castironcoding.com>
2025-10-08 15:29:20 -0700

    [C] Disable fetching manual lists for now
    It is causing some N+1 querying that add up over time for very large
    collections and is not presently being used

--- app/services/templates/entity_lists/manual_resolver.rb
+++ app/services/templates/entity_lists/manual_resolver.rb
@@ -7,9 +7,14 @@ module Templates
       option :manual_list_name, Templates::Types::String.optional, optional: true
 
       def resolve_entities
-        list_name = yield Maybe(manual_list_name)
+        # TODO: re-implement when we have manual lists
+        # This has been disabled for now because it
+        # causes slight performance issues for no benefit
+        # list_name = yield Maybe(manual_list_name)
 
-        Success source_entity.manual_list_entries.currently_visible.where(template_kind:, list_name:).limit(limit).map(&:target)
+        # source_entity.manual_list_entries.currently_visible.where(template_kind:, list_name:).limit(limit).map(&:target)
+
+        Success([])
       end
     end
   end
