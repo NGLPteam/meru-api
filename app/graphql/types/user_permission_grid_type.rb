2925c762d625fc8787622e39619076f68d049109
Alexa Grey <alexa@castironcoding.com>
2026-05-12 19:17:25 -0700

    [C] Ensure user permission grid implements grid interfaces

--- app/graphql/types/user_permission_grid_type.rb
+++ app/graphql/types/user_permission_grid_type.rb
@@ -2,5 +2,11 @@
 
 module Types
   class UserPermissionGridType < Types::BaseObject
+    implements Types::PermissionGridType
+    implements Types::CRUDPermissionGridType
+
+    description <<~TEXT
+    A grid of permissions related to user management in Meru.
+    TEXT
   end
 end
